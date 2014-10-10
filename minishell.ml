ed2dbccea50c88416524ee239b026ac0b3f274d3
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-09 22:45:59 -0400

    Make split tail-recursive

--- minishell.ml
+++ minishell.ml
@@ -7,20 +7,22 @@ let arg_parse line =
     then skip_blanks (i+1)
     else i
   in
-  let rec split start i =
+
+  let rec split accum start i =
     if i >= String.length line then
       if start = i then
-        []
+        accum
       else
-        [String.sub line start (i-start)]
+        String.sub line start (i-start) :: accum
     else if line.[i] = ' ' then
       let j = skip_blanks i in
-      String.sub line start (i-start) :: split j j
+      split (String.sub line start (i-start) :: accum) j j
     else
-      split start (i+1)
+      split accum start (i+1)
   in
+
   let j = skip_blanks 0 in
-  split j j
+  List.rev (split [] j j)
 
 let process_line line =
   let open Unix in
