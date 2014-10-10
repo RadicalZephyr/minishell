bf94c4ece57ea2b08db503060f14538354784cb4
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-09 20:20:48 -0400

    Get a basic read and print loop working

--- minishell.ml
+++ minishell.ml
@@ -2,7 +2,21 @@ open Core.Std
 
 
 let process_line line =
-  line
-
-let main () =
   ()
+
+
+let prepare_line line =
+  ()
+
+let rec prompt () =
+  Out_channel.output_string stderr "% ";
+  Out_channel.flush stderr;
+  match (In_channel.input_line stdin) with
+  | None -> ()
+  | Some line ->
+     process_line line;
+     prompt ()
+
+(* Launch the prompt function as main *)
+let () =
+  prompt ()
