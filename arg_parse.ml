37ccdd15ccf0bb38ff8bc819ab52e64628d674f4
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-18 15:34:57 -0400

    Pull out a function for appending to the buffer
    Allow the use of a more natural syntax for specifying the substring to
    select

--- arg_parse.ml
+++ arg_parse.ml
@@ -24,8 +24,12 @@ let process line =
   in
 
   let rec split accum buffer start i =
+    let append_to_buffer first last =
+      Buffer.add_substring buffer line first (last-first)
+    in
+
     if i >= String.length line then begin
-        Buffer.add_substring buffer line start (i-start);
+        append_to_buffer start i;
         let arg = Buffer.contents buffer in
         if String.length arg = 0 then accum
         else arg :: accum
@@ -35,16 +39,16 @@ let process line =
       | ' ' ->
          let j = skip_blanks i in
 
-         Buffer.add_substring buffer line start (i-start);
+         append_to_buffer start i;
          split ((Buffer.contents buffer) :: accum) (Buffer.create 10) j j
 
       | '"' ->
          let j = find_quote (i+1) in
          (* Copy the arg up to the starting quote *)
-         Buffer.add_substring buffer line start (i-start);
+         append_to_buffer start i;
 
          (* Copy the characters between the quotations *)
-         Buffer.add_substring buffer line (i+1) ((j-1)-i);
+         append_to_buffer (i+1) j;
 
          (* ... and reset the start pointer to after both strings *)
          split accum buffer (j+1) (j+1)
