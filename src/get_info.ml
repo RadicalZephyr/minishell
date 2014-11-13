6fe6308d838bf24917020a0fd6981db271b31ed0
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-13 17:20:18 -0500

    Replace ptr char with strings

--- src/get_info.ml
+++ src/get_info.ml
@@ -6,15 +6,15 @@ open Foreign
 
 type struct_passwd
 let struct_passwd : struct_passwd structure typ = structure "passwd"
-let pw_name   = field struct_passwd "pw_name"   (ptr char)
-let pw_passwd = field struct_passwd "pw_passwd" (ptr char)
+let pw_name   = field struct_passwd "pw_name"   string
+let pw_passwd = field struct_passwd "pw_passwd" string
 let pw_uid    = field struct_passwd "pw_uid"    uid_t
 let pw_gid    = field struct_passwd "pw_gid"    gid_t
 let pw_change = field struct_passwd "pw_change" time_t
-let pw_class  = field struct_passwd "pw_class"  (ptr char)
-let pw_gecos  = field struct_passwd "pw_gecos"  (ptr char)
-let pw_dir    = field struct_passwd "pw_dir"    (ptr char)
-let pw_shell  = field struct_passwd "pw_shell"  (ptr char)
+let pw_class  = field struct_passwd "pw_class"  string
+let pw_gecos  = field struct_passwd "pw_gecos"  string
+let pw_dir    = field struct_passwd "pw_dir"    string
+let pw_shell  = field struct_passwd "pw_shell"  string
 let pw_expire = field struct_passwd "pw_expire" time_t
 let pw_fields = field struct_passwd "pw_fields" int
 let () = seal struct_passwd
@@ -28,10 +28,10 @@ let getpwuid =
 
 type struct_group
 let struct_group : struct_group structure typ = structure "group"
-let gr_name   = field struct_group "gr_name"   (ptr char)
-let gr_passwd = field struct_group "gr_passwd" (ptr char)
+let gr_name   = field struct_group "gr_name"   string
+let gr_passwd = field struct_group "gr_passwd" string
 let gr_gid    = field struct_group "gr_gid"    gid_t
-let gr_mem    = field struct_group "gr_mem"    (ptr (ptr char))
+let gr_mem    = field struct_group "gr_mem"    (ptr string)
 let () = seal struct_group
 
 (*
